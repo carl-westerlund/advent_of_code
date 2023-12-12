3c899469bb6da7cf3ad078fed0d7aad026bab380
carl-westerlund <23073256+carl-westerlund@users.noreply.github.com>
2023-12-12 21:05:24 +0100

    Add solutions to day 12 2023

---
+++ 2023/day12/12a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+def num_arrs(arr, sizes, size_index):
+    if size_index == len(sizes):
+        if not '#' in arr:
+            return 1
+        else:
+            return 0
+    if arr == "" and size_index < len(sizes):
+        return 0
+    result = 0
+    if arr[0] in ".?":
+        result += num_arrs(arr[1:], sizes, size_index)
+    size = sizes[size_index]
+    if len(arr) >= size and all([x in "#?" for x in arr[:size]]) and (len(arr) == size or arr[size] != '#'):
+        result += num_arrs(arr[size+1:], sizes, size_index+1)
+    return result
+
+
+result = 0
+for line in sys.stdin:
+    [arr, sizes] = line.split()
+    result += num_arrs(arr, list(map(int, sizes.split(','))),0)
+print(result)
