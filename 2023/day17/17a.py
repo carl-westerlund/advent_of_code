e8a516407ed30557b5ea714c1aa9e688f9cee3fc
carl-westerlund <23073256+carl-westerlund@users.noreply.github.com>
2023-12-17 10:58:50 +0100

    Add solutions to day 17 2023

---
+++ 2023/day17/17a.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import sys
+import heapq
+
+def move(r, c, d):
+    match d:
+        case 'n':
+            return (r-1, c)
+        case 's':
+            return (r+1, c)
+        case 'w':
+            return (r, c-1)
+        case 'e':
+            return (r, c+1)
+
+mp = list(map(lambda line: list(map(int, list(line.rstrip()))), sys.stdin.readlines()))
+rows = len(mp)
+cols = len(mp[0])
+
+dirs = "nwse"
+visited = {d : [[False]*cols for _ in range(rows)] for d in dirs}
+queue = [(0,0,0,'e'), (0,0,0,'s')]  # heat loss so far, row, column, direction
+while queue:
+    (loss, r, c, d) = heapq.heappop(queue)
+    if (r,c) == (rows-1, cols-1):
+        break
+    if visited[d][r][c]:
+        continue
+    visited[d][r][c] = True
+    for i in range(3):
+        (r,c) = move(r,c,d)
+        if r < 0 or r >= rows or c < 0 or c >= cols:
+            break
+        loss += mp[r][c]
+        for new_d in ("ns" if d in "we" else "we"):
+            heapq.heappush(queue, (loss, r, c, new_d))
+
+print(loss)
