a83a344c6129d9e6fa399d22b2962e453207df1d
carl-westerlund <23073256+carl-westerlund@users.noreply.github.com>
2024-01-01 15:39:43 +0100

    Add solutions to day 18 2023

---
+++ 2023/day18/18b.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import sys
+
+# The shoelace formula assumes that the polygon is simple,
+# which is true in the test case, but it isn't always true.
+
+def move(r, c, d, dist):
+    match d:
+        case 'U':
+            return (r - dist, c)
+        case 'D':
+            return (r + dist, c)
+        case 'L':
+            return (r, c - dist)
+        case 'R':
+            return (r, c + dist)
+
+(r,c) = (0,0)
+pts = [(0,0)]
+for line in sys.stdin:
+    [_, _, color_code] = line.rstrip().split()
+    color_code = color_code.strip("()#")
+    dist = int(color_code[0:5], 16)
+    d = "RDLU"[int(color_code[5])]
+    (r, c) = move(r, c, d, dist)
+    pts.append((r,c))
+
+pts.append(pts[-1])
+path_length = 0
+area = 0
+for i in range(len(pts)-1):
+    path_length += abs(pts[i][0] - pts[i+1][0]) + abs(pts[i][1] - pts[i+1][1])
+    area += pts[i][0]*pts[i+1][1] - pts[i+1][0]*pts[i][1]
+
+area = abs(area)
+area = (area + path_length) // 2 + 1
+print(area)
